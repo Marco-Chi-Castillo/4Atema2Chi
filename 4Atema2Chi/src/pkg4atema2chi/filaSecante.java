4d2078b0ec958400aab3b716912502a35bf5e2bb
Marco Antonio Chi Castillo <chimarcoantonio@gmail.com>
2019-03-08 17:25:46 -0600

    Solucion de problema Secante

--- 4Atema2Chi/src/pkg4atema2chi/filaSecante.java
+++ 4Atema2Chi/src/pkg4atema2chi/filaSecante.java
@@ -10,5 +10,71 @@ package pkg4atema2chi;
  * @author dell
  */
 public class filaSecante {
+    private int i;
+    private double xi1,xi,fxi1,fxi,formula1,formula2,erro;
+
     
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public void setXi1(double xi1) {
+        this.xi1 = xi1;
+    }
+
+    public void setXi(double xi) {
+        this.xi = xi;
+    }
+
+    public void setFxi1(double fxi1) {
+        this.fxi1 = fxi1;
+    }
+
+    public void setFxi(double fxi) {
+        this.fxi = fxi;
+    }
+
+    public void setFormula1(double formula1) {
+        this.formula1 = formula1;
+    }
+
+    public void setFormula2(double formula2) {
+        this.formula2 = formula2;
+    }
+
+    public void setErro(double erro) {
+        this.erro = erro;
+    }
+    
+    public int getI() {
+        return i;
+    }
+
+    public double getXi1() {
+        return xi1;
+    }
+
+    public double getXi() {
+        return xi;
+    }
+
+    public double getFxi1() {
+        return fxi1;
+    }
+
+    public double getFxi() {
+        return fxi;
+    }
+
+    public double getFormula1() {
+        return formula1;
+    }
+
+    public double getFormula2() {
+        return formula2;
+    }
+
+    public double getErro() {
+        return erro;
+    }
 }
